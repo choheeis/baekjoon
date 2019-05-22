9cc2c02c5bf7ee43c734e73667aa43a14c9fc85c
choheeis <chchgml10@gmail.com>
2019-05-22 22:53:33 +0900

    05/22 백준

--- 1834번 - 나머지와 몫이 같은 수.cpp
+++ 1834번 - 나머지와 몫이 같은 수.cpp
@@ -2,10 +2,15 @@
 
 int main(){
 	int n;
-	long long num = 0;
+	long long int num = 0;
 	scanf("%d", &n);
 	for(int i = 1 ; i < n ; i++){
-		num = num + ((long long)n*(long long)i+(long long)i);
+		num = num + ((long long int)n*(long long int)i+(long long int)i);
 	}
 	printf("%lld", num);
 } 
+
+
+
+
+
